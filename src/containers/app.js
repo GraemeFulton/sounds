f030de1559acdbb79d50dc03e086a8137659b589
Graeme Fulton <graemefulton89@gmail.com>
2017-08-21 10:10:50 +0200

    background height

--- src/containers/app.js
+++ src/containers/app.js
@@ -58,19 +58,20 @@ export class AppContainer extends Component {
     }
 
     return (
-      <div className={'container'}>
-        <section className={'col-sm-12 text-center container'} style={{ color: '#999' }}>
+      <div>
+      <div className={'col-sm-12'}>
+        <section className={'text-center'} style={{ color: '#999' }}>
           <div style={{ marginBottom: '10px', marginTop: '20px', fontFamily: 'Euphoria Script, cursive' }}>
             <p>ASMR.SH</p>
           </div>
-          <section className={'col-sm-6'} style={{ margin: '0 auto', padding: '0px', marginTop: '30px', opacity: '0.3', width: '516px' }}>
-            <ReactPlayer url={this.state.sound} playing height="290" width="516" />
-          </section>
-          <div style={{ margin: '0px auto', padding: '12px', marginTop: '30px', position: 'relative' }}>
+          <div className={'col-sm-12 text-center'}>
+            <ReactPlayer url={this.state.sound} playing height="290" width="516" style={{ margin: '0 auto' }} />
+          </div>
+          <div className={'col-sm-12'} style={{ marginTop:' 20px' }}>
             <h1 style={{ fontFamily: 'Euphoria Script, cursive' }}>Playing Today:&nbsp;
               <a rel="noopener noreferrer" target="_blank" style={{ textDecoration: 'underline', color: '#999' }} href={this.state.sound}>{this.state.title}</a>
             </h1>
-            <h2 className={'col-xs-12'} style={{ fontFamily: 'Taviraj, serif', fontSize: '1.2rem', margin: '4px auto', display: 'inline-block' }}>
+            <h2 className={'col-sm-12'} style={{ fontFamily: 'Taviraj, serif', fontSize: '1.2rem', margin: '4px auto', display: 'inline-block' }}>
               <span className={'prefix'} style={{ display: 'inline-block' }}>Come back</span> <span style={{ display: 'inline-block' }}>in&nbsp;</span>
               <div style={{ display: 'inline-block' }}>
                 <Countdown options={OPTIONS} />
@@ -79,7 +80,7 @@ export class AppContainer extends Component {
             <p style={{ width: '50px', margin: '10px auto' }}><a target="_blank" href="https://twitter.com/intent/tweet?text=Come%20and%20listen%20to%20sounds%20on%20@ASMRSH1">
               <img alt="tweet" style={{ width: '30px', opacity: '0.5' }} src="http://eglx.ca/wp-content/uploads/2016/01/Twitter-icon.png" /></a></p>
           </div>
-          <section className={'col-sm-6'} style={{ margin: '0 auto', display: 'none' }}>
+          <div style={{ display: 'none' }}>
             <Timeline
               dataSource={{
                 sourceType: 'profile',
@@ -91,19 +92,15 @@ export class AppContainer extends Component {
               }}
               onLoad={() => this.setSound()}
             />
-          </section>
-
-          <div
-            className={'col-sm-6 col-sm-offset-6 shadow'}
-            style={{ margin: '0px auto', left: '50%', transform: 'translate(-50%, 0)', background: 'rgba(13,8,42,1)',
-              borderRadius: '6px', padding: '8px', position: 'fixed', bottom: '0px', borderBottomLeftRadius: '0px', borderBottomRightRadius: '0px'
-            }}
-          >
-            <div id="mc_embed_signup">
+        </div>
+      </section>
+    </div>
+          <div style={{ width: '100vw', position: 'fixed', bottom: '0px' }}>
+            <div id="mc_embed_signup" className={'shadow'} style={{ background: 'rgba(13,8,42,1)', borderRadius: '6px', padding: '8px', borderBottomLeftRadius: '0px', borderBottomRightRadius: '0px', margin: '0 auto' }}>
               <form style={{ color: '#999' }} action="//asmr.us16.list-manage.com/subscribe/post?u=d8b01ee10bb4c297f025a9729&amp;id=3d0223a8f6" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" className="validate" target="_blank" noValidate>
                 <div id="mc_embed_signup_scroll">
                   <label htmlFor="mce-EMAIL" style={{ color: '#7d67ae' }}>🎙 Get the weekly playlist</label>
-                  <input type="email" name="EMAIL" className="email" id="mce-EMAIL" placeholder="Enter email address (and listen to your keyboard)" required style={{ background: '#111', color: '#999', borderColor: '#220045' }} />
+                  <input type="email" name="EMAIL" className="email" id="mce-EMAIL" placeholder="Enter email address (and listen to your keyboard)" required style={{ background: '#111', color: '#999', borderColor: '#54437c' }} />
                   <div style={{ position: 'absolute', left: '-5000px' }} ariaHidden="true">
                     <input type="text" name="b_d8b01ee10bb4c297f025a9729_3d0223a8f6" tabIndex="-1" />
                   </div>
@@ -114,8 +111,7 @@ export class AppContainer extends Component {
               </form>
             </div>
           </div>
-        </section>
-      </div>
+        </div>
     );
   }
 }
